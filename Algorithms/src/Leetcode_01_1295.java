335e07060d97394c607a12aa9650b5c126233d95
Nguyen Van Nghia <nghia.nguyen@tp.com.vn>
2022-10-03 22:20:13 +0700

    Algorithms leetcode day 1

---
+++ Algorithms/src/Leetcode_01_1295.java
@@ -0,0 +1,30 @@
+public class Leetcode_01_1295 {
+    public static int findNumbers(int[] nums) {
+        int resultCount = 0;
+        for (int item: nums) {
+            // kiem tra so chu so cua item
+            // Neu so chu so la chan => tang bien dem len 1
+            int soLuongChuSo = tinhSoChuSo(item);
+            if (soLuongChuSo % 2 == 0) {
+                resultCount++;
+            }
+        }
+        return resultCount;
+    }
+
+    private static int tinhSoChuSo(int item) {
+        int count = 0;
+        int result = item;
+        while (result != 0) {
+            result = item / 10;
+            item = result;
+            count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] number = {1, 22, 3, 44, 5, 66};
+        System.out.println(findNumbers(number));
+    }
+}
---
+++ Algorithms/src/Leetcode_01a_1295.java
@@ -0,0 +1,26 @@
+public class Leetcode_01a_1295 {
+
+    private static int findNumbers(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            if (evenDigits(num)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static boolean evenDigits(int number) {
+        int noofdigits = digits(number);
+        return noofdigits % 2 == 0;
+    }
+
+    private static int digits(int number) {
+        return (int) (Math.log10(number) + 1);
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 22, 3, 44, 5, 66, 23456, 45678};
+        System.out.println(findNumbers(array));
+    }
+}
