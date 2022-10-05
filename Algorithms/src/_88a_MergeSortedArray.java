2ad2e6b74614600486d918a41aa320b0a8e1d6b9
Nguyen Van Nghia <nghia.nguyen@tp.com.vn>
2022-10-05 21:30:58 +0700

    Algorithms leetcode day 3

---
+++ Algorithms/src/_88a_MergeSortedArray.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class _88a_MergeSortedArray {
+    public static void merge(int[] n1, int m, int[] n2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while (k >= 0) {
+            if (j < 0) {
+                n1[k] = n1[i];
+                i--;
+            } else if (i < 0) {
+                n1[k] = n2[j];
+                j--;
+            } else if (n1[i] > n2[j]) {
+                n1[k] = n1[i];
+                i--;
+            } else {
+                n1[k] = n2[j];
+                j--;
+            }
+            k--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] n1 = {2, 3, 4, 5, 0, 0, 0};
+        int[] n2 = {0, 3, 6};
+        merge(n1, 4, n2, 3);
+        System.out.println(Arrays.toString(n1));
+        System.out.println("DONE");
+    }
+}
